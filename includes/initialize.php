281d3447a6127142cb385c10f2af586400b139dc
LeeaJames <51928201+LeeaJames@users.noreply.github.com>
2019-07-17 22:04:06 +0100

    Delete initialize.php

--- includes/initialize.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include 'includes/dbh.inc.php';
-include 'includes/user.inc.php';
-include 'includes/viewuser.inc.php';
-include 'includes/packages.php';
-include 'includes/viewPackages.php';
-?>
